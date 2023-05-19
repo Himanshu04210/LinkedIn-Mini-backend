22194a0ecf5dd20c77082968746e4af91b50b4a5
Himanshu04210 <himanshupoly76075@gmail.com>
2023-05-19 21:40:14 +0530

    created the structure of the project

---
+++ LinkedIn_backend/src/main/java/com/linkedIn/LinkedInBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkedIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedInBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedInBackendApplication.class, args);
+	}
+
+}
