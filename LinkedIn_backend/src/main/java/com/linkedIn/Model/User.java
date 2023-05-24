a6e219f854e72260a97a6839c63560f1df5b3435
Himanshu04210 <himanshupoly76075@gmail.com>
2023-05-24 13:23:09 +0530

    Creating the entity classes

--- LinkedIn_backend/src/main/java/com/linkedIn/Model/User.java
+++ LinkedIn_backend/src/main/java/com/linkedIn/Model/User.java
@@ -1,5 +1,17 @@
 package com.linkedIn.Model;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+	private String name;
+	@Column(unique = true)
+	private String userName;
 }
